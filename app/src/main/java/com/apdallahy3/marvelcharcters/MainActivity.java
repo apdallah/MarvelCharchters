303cee83146e76e3fc46b41401c353d6cb356de5
Abdallah <Ab21111392>
2019-01-20 19:08:00 +0200

    Initial Commit

---
+++ app/src/main/java/com/apdallahy3/marvelcharcters/MainActivity.java
@@ -0,0 +1,13 @@
+package com.apdallahy3.marvelcharcters;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
